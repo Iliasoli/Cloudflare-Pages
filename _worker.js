623761bc6d9fee88c14c07548b0945a2831fd779
Iliasoli <105164394+Iliasoli@users.noreply.github.com>
2023-12-09 15:23:03 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'https://affectionatedesertedplots.iliasoleimany.repl.co/745f7f61-f5ce-4b56-ae02-9edaa99ab5c9.html'
+      url.hostname = 'affectionatedesertedplots.iliasoleimany.repl.co/745f7f61-f5ce-4b56-ae02-9edaa99ab5c9.html'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
