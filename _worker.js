2d6ba2d5c40ea5328231f15c071f9ae337e22096
Iliasoli <105164394+Iliasoli@users.noreply.github.com>
2023-12-09 14:31:19 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.hicairo.com'
+      url.hostname = 'yj3yd4-8080.csb.app/d008d104-a045-4d11-9967-fdd5f9aeede8.html'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
